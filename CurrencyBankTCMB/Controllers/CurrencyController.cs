7885bb458b36867e12aca0001041e5095f81c56d
Umut Ocak <48753655+umutocak@users.noreply.github.com>
2024-05-23 00:42:35 +0300

    Add project files.

---
+++ CurrencyBankTCMB/Controllers/CurrencyController.cs
@@ -0,0 +1,33 @@
+﻿using CurrencyBankTCMB.models;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml;
+
+namespace CurrencyBankTCMB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+
+        [HttpGet("get-currency")]
+        public IActionResult GetCurrency()
+        {
+            XmlDocument doc1 = new XmlDocument();
+            doc1.Load("https://www.tcmb.gov.tr/kurlar/today.xml");
+            XmlElement root = doc1.DocumentElement!;
+            XmlNodeList nodes = root.SelectNodes("/Tarih_Date/Currency")!;
+            List<Currency> currencies = new List<Currency>();
+            foreach (XmlNode node in nodes)
+            {
+                Currency currency = new Currency();
+                currency.Code = node.Attributes!["Kod"]!.Value;
+                currency.ForexBuying = node["ForexBuying"]!.InnerText;
+                currency.ForexSelling = node["ForexSelling"]!.InnerText;
+                currency.BanknoteBuying = node["BanknoteBuying"]!.InnerText;
+                currency.BanknoteSelling = node["BanknoteSelling"]!.InnerText;
+                currencies.Add(currency);
+            }
+            return currencies != null ? Ok(currencies) : BadRequest(currencies);
+        }
+    }
+}
---
+++ CurrencyBankTCMB/models/Currency.cs
@@ -0,0 +1,11 @@
+﻿namespace CurrencyBankTCMB.models
+{
+    public class Currency
+    {
+        public string? Code { get; set; }
+        public string? ForexBuying { get; set; }
+        public string? ForexSelling { get; set; }
+        public string? BanknoteBuying { get; set; }
+        public string? BanknoteSelling { get; set; }
+    }
+}
